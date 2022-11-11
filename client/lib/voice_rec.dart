cfc2993a1ff25636bff0adef191543c1732af59b
unknown <aishwaryaaj2002@gmail.com>
2022-11-11 20:12:11 +0530

    obj_mlkit

--- client/lib/voice_rec.dart
+++ client/lib/voice_rec.dart
@@ -1,26 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
-
-class STT{
-  SpeechToText _speechToText = SpeechToText();
-  bool _speechEnabled = false;
-  String command_word = 'default';
-  STT(){
-    _initSpeech();
-  }
-  void _initSpeech() async {
-    _speechEnabled = await _speechToText.initialize();
-  }
-  Future startListening() async {
-    await _speechToText.listen(onResult: _onSpeechResult);
-  }
-  void _stopListening() async {
-    await _speechToText.stop();
-  }
-  void _onSpeechResult(SpeechRecognitionResult result) {
-    command_word=result.recognizedWords;
-    // print(command_word);
-
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:speech_to_text/speech_recognition_result.dart';
+// import 'package:speech_to_text/speech_to_text.dart';
+//
+// class STT{
+//   SpeechToText _speechToText = SpeechToText();
+//   bool _speechEnabled = false;
+//   String command_word = 'default';
+//   STT(){
+//     _initSpeech();
+//   }
+//   void _initSpeech() async {
+//     _speechEnabled = await _speechToText.initialize();
+//   }
+//   Future startListening() async {
+//     await _speechToText.listen(onResult: _onSpeechResult);
+//   }
+//   void _stopListening() async {
+//     await _speechToText.stop();
+//   }
+//   void _onSpeechResult(SpeechRecognitionResult result) {
+//     command_word=result.recognizedWords;
+//     // print(command_word);
+//
+//   }
+// }
