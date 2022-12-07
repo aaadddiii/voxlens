33c1c1c18790e9dcf4ebc0991df97d4b25c20495
aaadddiii <adithya.kurien@gmail.com>
2022-12-07 20:16:07 +0530

    fixes

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,12 +4,17 @@ import 'home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'locator.dart';
 import 'package:client/face_recognition/camera_service.dart';
+import 'package:flutter/services.dart';
+
 
 List<CameraDescription> cameras = [];
 Future main() async {
   setupServices();
   await dotenv.load(fileName: ".env");
   cameras = await availableCameras();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MaterialApp(
       theme: ThemeData(
           appBarTheme: AppBarTheme(
