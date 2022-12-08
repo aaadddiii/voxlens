de9934d41e2838f7e015e344ffccd1609e8f2d57
Aleena-Ashraf <56963576+Aleena-Ashraf@users.noreply.github.com>
2022-12-08 09:04:17 +0530

    data objects

--- client/lib/distance_calculation/object_detector_painter.dart
+++ client/lib/distance_calculation/object_detector_painter.dart
@@ -19,7 +19,6 @@ class ObjectDetectorPainter extends CustomPainter {
       // "distance": 0.5
       "ratio": 36382.5
     },
-
     "Tableware": {
       // "cornerPoints": [284.0, 357.0, 431.0, 852.0],
       // "distance": 0.5
@@ -33,29 +32,53 @@ class ObjectDetectorPainter extends CustomPainter {
     "Person": {
       // "cornerPoints" : [196.0, 298.0, 496.0, 1206.0],
       // "distance" : 2.0
+      "ratio" : 544800.0
     },
-     "Bench" : {
+    "Bench" : {
       // "cornerPoints" : [73.0, 30.0, 577.0, 419.0],
       // "distance" : 3.5
-  },
+      "ratio" : 686196.0
+    },
     "Chair" : {
       // "cornerPoints" : [165.0, 133.0, 446.0, 556.0],
       // "distance" : 2
-  },
-   "Backpack" : {
+      "ratio" : 237726.0
+    },
+    "Backpack" : {
       // "cornerPoints" : [280.0, 797.0, 482.0, 1070.0],
       // "distance" : 2
-  },
+      "ratio" : 110292.0
+
+    },
     "Pen" : {
       // "cornerPoints" : [257.0, 547.0, 315.0, 878.0],
       // "distance" : 0.5
+      "ratio" : 65308.0
     },
-     "Notebook" : {
+    "Notebook" : {
       // "cornerPoints" : [167.0, 269.0, 720.0, 1193.0],
       // "distance" : 0.5
-  }
-  };
+      "ratio" : 255486.0
+    },
+    "Table" :{
+      //"cornerPoints":[21.0, 318.0, 663.0, 849.0],
+      //"distance":1.5
+      "ratio" :511353.0
+    },
 
+    "Mobile phone" :{
+      //"cornerPoints":[290.0, 630.0, 413.0, 844.0],
+      //"distance":0.75
+      "ratio" :19741.5
+    },
+
+    "Laptop" :{
+      //"cornerPoints":[140.0, 151.0, 610.0, 450.0],
+      //"distance":0.75
+      "ratio" :105397.5
+    }
+
+  };
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint()
